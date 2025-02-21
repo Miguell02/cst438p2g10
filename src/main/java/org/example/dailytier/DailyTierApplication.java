7ec7ebaf511d31b3b2b4db2f1fb38c82e1efd9d9
Miguell02 <miguelgonzalez1@csumb.edu>
2025-02-20 17:40:52 -0800

    initial commit

---
+++ src/main/java/org/example/dailytier/DailyTierApplication.java
@@ -0,0 +1,13 @@
+package org.example.dailytier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyTierApplication.class, args);
+    }
+
+}
